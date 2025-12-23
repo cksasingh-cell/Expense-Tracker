767f37c7cc0faf517ee3cd433fdb03da0c102810
cksasingh-cell <cksasingh@gmail.com>
2025-12-23 15:04:19 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+// Service Worker for Expense Tracker PWA
+const CACHE_NAME = 'expense-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
